9415d7cc4595684af4bc0f3b3d56b809499d6744
Xiomara S <42756313+XioSawin@users.noreply.github.com>
2021-08-13 00:04:25 -0300

    Update mensajesController.js

--- controllers/mensajesController.js
+++ controllers/mensajesController.js
@@ -28,31 +28,7 @@ app.use(express.static('public'));
 
 const messagesModel = require('../models/mensajes');
 
-/*
-const getMessages = () => {
-    const messages = messagesModel.find({});
 
-    return messages;
-}
-
-io.on('connection', (socket) => {
-    socket.emit('messages', getMessages());
-
-    socket.on('new-message', function(data) {
-
-        const messageSaved = new messagesModel(data);
-
-        messageSaved.save()
-            .then( () => res.sendStatus(201) )
-            .catch( (err) => res.status(400).json({
-                status: 400,
-                message: 'Error guardando mensaje en base de datos'
-            }));
-        
-        io.sockets.emit('messages', getMessages());
-    })
-})
-*/
 const getChat = async(req, res) => {
     res.render("chat", {});
 }
